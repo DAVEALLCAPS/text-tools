a315e2aca3a8c220e6e954f79a8f6bcb5eea2aa2
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2023-09-29 19:17:19 -0600

    Create initial version of InputTextBox component

--- src/components/InputTextBox.js
+++ src/components/InputTextBox.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
 export default function InputTextBox() {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
-    <div>InputTextBox</div>
-  )
+    <div>
+      <textarea
+        value={inputText}
+        onChange={handleChange}
+        placeholder="Paste or type your text here..."
+        rows="20"
+        cols="100"
+      ></textarea>
+    </div>
+  );
 }
