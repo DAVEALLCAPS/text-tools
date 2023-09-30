1f10d358ad4660d8804aeb168332be243d72162c
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2023-09-30 09:34:42 -0600

    feat: add text statistics to InputTextBox component

--- src/components/InputTextBox.js
+++ src/components/InputTextBox.js
@@ -5,6 +5,10 @@ function InputTextBox({ inputText, setInputText }) {
     setInputText(event.target.value);
   };
 
+  const characterCount = inputText.length;
+  const wordCount = inputText.split(/\s+/).filter(Boolean).length;
+  const textSize = new Blob([inputText]).size;
+
   return (
     <div className="mt-1">
       <label className="block text-lg font-bold mb-2" htmlFor="inputTextArea">Input Text</label>
@@ -17,6 +21,11 @@ function InputTextBox({ inputText, setInputText }) {
         cols="50"
         className="p-2 mb-2 border rounded"
       ></textarea>
+      <div className="text-sm mt-2">
+        <p>Character Count: {characterCount}</p>
+        <p>Word Count: {wordCount}</p>
+        <p>Text Size (bytes): {textSize}</p>
+      </div>
     </div>
   );
 }
