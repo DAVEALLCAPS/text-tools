503ff967192c35e83564fe460ee0e2c114819875
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2023-09-29 19:01:15 -0600

    Initialize App component structure with comments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    {/* Input Text Box Component */}
+    {/* This component will be responsible for taking user input */}
+    
+    {/* Text Options Component */}
+    {/* This component will contain various options for text processing */}
+
+    {/* Output Text Box Component */}
+    {/* This component will display the processed text and have the option to copy to clipboard */}
+  </div>
   );
 }
 
