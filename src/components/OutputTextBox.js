a8e891f22482b56eee179a5aaf47581261a83094
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2023-09-29 19:17:34 -0600

    Create initial version of OutputTextBox component

--- src/components/OutputTextBox.js
+++ src/components/OutputTextBox.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function OutputTextBox() {
+export default function OutputTextBox({ newText }) {
+  const [outputText, setOutputText] = useState("");
+
+  useEffect(() => {
+    setOutputText(newText);
+  }, [newText]);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(outputText);
+    alert("Text copied to clipboard");
+  };
+
   return (
-    <div>OutputTextBox</div>
-  )
+    <div>
+      <textarea value={outputText} readOnly rows="20" cols="100"></textarea>
+      <button onClick={handleCopy}>Copy to Clipboard</button>
+    </div>
+  );
 }
