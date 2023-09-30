dd949630b18d7ee53d4f191ee8639f8cd159b532
DAVEALLCAPS <108239206+DAVEALLCAPS@users.noreply.github.com>
2023-09-29 19:06:10 -0600

    Integrate InputTextBox, TextOptions, and OutputTextBox into App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
+import "./App.css";
+import InputTextBox from "./components/InputTextBox";
+import OutputTextBox from "./components/OutputTextBox";
+import TextOptions from "./components/TextOptions";
 
 function App() {
   return (
     <div className="App">
-    {/* Input Text Box Component */}
-    {/* This component will be responsible for taking user input */}
-    
-    {/* Text Options Component */}
-    {/* This component will contain various options for text processing */}
-
-    {/* Output Text Box Component */}
-    {/* This component will display the processed text and have the option to copy to clipboard */}
-  </div>
+      <InputTextBox />
+      {/* Input Text Box Component */}
+      {/* This component will be responsible for taking user input */}
+      <TextOptions />
+      {/* Text Options Component */}
+      {/* This component will contain various options for text processing */}
+      <OutputTextBox />
+      {/* Output Text Box Component */}
+      {/* This component will display the processed text and have the option to copy to clipboard */}
+    </div>
   );
 }
 
