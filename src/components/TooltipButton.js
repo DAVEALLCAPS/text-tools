92d199443cc471c207cdc4daa408b8f2a05b2809
DAVEALLCAPS <daveallcaps@gmail.com>
2023-10-07 20:07:40 -0600

    Add tooltip, create TooltipButton component

---
+++ src/components/TooltipButton.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'; 
+import Button from './Button'; 
+
+const TooltipButton = ({ onClick, children, tooltipContent }) => {
+  return (
+    <Tooltip>
+      <TooltipTrigger>
+        <Button onClick={onClick}>
+          {children}
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>{tooltipContent}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+};
+
+export default TooltipButton;
