14e6b2744b3d55c9b0bda41d6e97b7ab95eec828
DAVEALLCAPS <daveallcaps@gmail.com>
2023-10-09 10:06:30 -0600

    Refactor App.js, Move Text Tool to TextToolPage, Create TTSPage

--- src/App.js
+++ src/App.js
@@ -1,60 +1,25 @@
-import React, { useState } from "react";
-import InputTextBox from "./components/InputTextBox";
-import OutputTextBox from "./components/OutputTextBox";
-import CopyButton from "./components/CopyButton";
-import JoinLines from "./components/TextOptions/JoinLines";
-import Uppercase from "./components/TextOptions/Uppercase";
-import Lowercase from "./components/TextOptions/Lowercase";
-import TextStatistics from "./components/TextStatistics";
-import Base64Decode from "./components/TextOptions/Base64Decode";
-import Base64Encode from "./components/TextOptions/Base64Encode";
-import { TooltipProvider } from "./components/ui/tooltip";
-import ExtractEmails from "./components/TextOptions/ExtractEmails";
-import ExtractURLs from "./components/TextOptions/ExtractURLs";
-import SaveToTXT from "./components/SaveToTXT";
-import SaveToCSV from "./components/SaveToCSV";
-import JsonFormatter from "./components/TextOptions/JsonFormatter";
-import JsonMinifier from "./components/TextOptions/JsonMinifier";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import TextToolPage from './TextToolPage';
+import TTSPage from './TTSPage'; 
 
 function App() {
-  const [inputText, setInputText] = useState("");
-  const [outputText, setOutputText] = useState("");
-
   return (
-    <TooltipProvider>
-      <div className="min-h-screen bg-gray-800 text-white grid grid-cols-2 gap-4 p-4">
-        <div>
-          <label htmlFor="input" className="block text-sm font-medium">
-            Input
-          </label>
-          <InputTextBox inputText={inputText} setInputText={setInputText} />
-          <TextStatistics inputText={inputText} />
-        </div>
-        <div>
-          <label htmlFor="output" className="block text-sm font-medium">
-            Output
-          </label>
-          <OutputTextBox outputText={outputText} />
-          <TextStatistics inputText={outputText} />
-        </div>
-        <div className="col-span-2 flex flex-wrap gap-4 justify-end">
-          <CopyButton textToCopy={outputText} />
-          <SaveToTXT text={outputText} />
-          <SaveToCSV text={outputText} />
-        </div>
-        <div className="col-span-2 flex gap-4 justify-center">
-          <JoinLines inputText={inputText} applyOption={setOutputText} />
-          <Base64Encode inputText={inputText} applyOption={setOutputText} />
-          <Base64Decode inputText={inputText} applyOption={setOutputText} />
-          <Uppercase inputText={inputText} applyOption={setOutputText} />
-          <Lowercase inputText={inputText} applyOption={setOutputText} />
-          <ExtractEmails inputText={inputText} applyOption={setOutputText} />
-          <ExtractURLs inputText={inputText} applyOption={setOutputText} />
-          <JsonFormatter inputText={inputText} applyOption={setOutputText} />
-          <JsonMinifier inputText={inputText} applyOption={setOutputText} />
-        </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Text Tool</Link></li>
+            <li><Link to="/tts">Text-to-Speech</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<TextToolPage />} />
+          <Route path="/tts" element={<TTSPage />} />
+        </Routes>
       </div>
-    </TooltipProvider>
+    </Router>
   );
 }
 
---
+++ src/TTSPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TTSPage() {
+  return (
+    <div>
+      <h2>Text-to-Speech Tool</h2>
+    </div>
+  );
+}
+
+export default TTSPage;
