cc591d24a5091d6116e2d8002e715d42dfbd7590
DAVEALLCAPS <daveallcaps@gmail.com>
2023-10-09 10:59:17 -0600

    Add TTS Rate Slider

--- src/TTSPage.js
+++ src/TTSPage.js
@@ -2,49 +2,68 @@ import React, { useState } from 'react';
 
 function TTSPage() {
   const [text, setText] = useState('');
+  const [rate, setRate] = useState(1);
 
   const speakText = () => {
-    // Create a new instance of SpeechSynthesisUtterance
     const msg = new SpeechSynthesisUtterance();
-    
-    // Set the text for the SpeechSynthesisUtterance
     msg.text = text;
-
-    // Use the speechSynthesis interface to speak the text
+    msg.rate = Number(rate);
     window.speechSynthesis.speak(msg);
   };
   
   const stopSpeech = () => {
-    // Cancel any ongoing speech synthesis
     window.speechSynthesis.cancel();
   }
 
   return (
-    <div className="min-h-screen bg-gray-800 p-4">
-      <label htmlFor="tts-input" className="block text-sm font-medium text-white">
+    <div className="min-h-screen bg-gray-800 p-4 text-white">
+      <label htmlFor="tts-input" className="block text-sm font-medium">
         Enter Text to Speak
       </label>
-      <input
-        type="text"
+      <textarea
         id="tts-input"
-        className="w-full p-2 rounded mt-2"
+        className="w-full p-2 rounded mt-2 text-black h-24"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        placeholder="Type text here to be converted to speech..."
+        aria-label="Text to Speech Input"
       />
-      <button
-        type="button"
-        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={speakText}
-      >
-        Speak Text
-      </button>
-      <button
-          type="button"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          onClick={stopSpeech}
-        >
-          Stop Speaking
-        </button>
+      <div className="mt-4 flex items-center">
+        <div className="w-1/3">
+          <label htmlFor="rate" className="block text-sm font-medium">
+            Speech Rate: {rate}x
+          </label>
+          <input 
+            type="range" 
+            id="rate" 
+            min="1" 
+            max="2" 
+            step="0.25" 
+            value={rate} 
+            onChange={(e) => setRate(e.target.value)}
+            className="w-full"
+            aria-label="Speech Rate Slider"
+          />
+        </div>
+        <div className="flex-grow flex space-x-4 ml-4">
+          <button
+            type="button"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex-grow"
+            onClick={speakText}
+            aria-label="Speak Text Button"
+          >
+            Speak Text
+          </button>
+          <button
+            type="button"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded flex-grow"
+            onClick={stopSpeech}
+            aria-label="Stop Speaking Button"
+          >
+            Stop Speaking
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
