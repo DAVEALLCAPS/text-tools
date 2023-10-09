dbaa373a5847bc187a4044bce90b6614685f9d58
DAVEALLCAPS <daveallcaps@gmail.com>
2023-10-09 10:36:49 -0600

    Add navigation-menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TextToolPage from './TextToolPage';
-import TTSPage from './TTSPage'; 
+import TTSPage from './TTSPage';
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger
+} from './components/ui/navigation-menu';
 
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Text Tool</Link></li>
-            <li><Link to="/tts">Text-to-Speech</Link></li>
-          </ul>
-        </nav>
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Tools</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid gap-3 p-6">
+                <li>
+                  <NavigationMenuLink href="/">Text Manipulation</NavigationMenuLink>
+                </li>
+                <li>
+                  <NavigationMenuLink href="/tts">Text to Speech</NavigationMenuLink>
+                </li>
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
 
-        <Routes>
-          <Route path="/" element={<TextToolPage />} />
-          <Route path="/tts" element={<TTSPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<TextToolPage />} />
+        <Route path="/tts" element={<TTSPage />} />
+      </Routes>
     </Router>
   );
 }
